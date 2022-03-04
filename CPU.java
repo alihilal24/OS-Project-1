58494ab85bd87df142004e4efbd0c5caf3ae9975
Ali Hilal <alihilal24@gmail.com>
2022-03-03 20:35:37 -0600

    Initial Commit

---
+++ CPU.java
@@ -0,0 +1,13 @@
+/*
+Registers: PC SP IR AC X Y
+Runs user program at address 0
+Intructions fetched from memory --> IR (Operand can be put in local variable)
+Each instruction is exccuted before the next instruction  is called
+user stack at the end of user  memory
+system stack at the end of  system memory
+user program should not  access system memory
+*/
+
+public class CPU {
+    
+}
